a4a95733a5c5cf238dad14b9520524f7bcfed233
MateusLiraFront <mlgtifront@gmail.com>
2025-09-02 11:10:12 -0300

    funções

--- fundamentos/src/components/Events.js
+++ fundamentos/src/components/Events.js
@@ -3,6 +3,14 @@ const Events = () => {
         console.log(e);
     };
 
+    const renderSomething = (x) =>{
+        if (x){
+            return <h1>Renderizando isso!</h1>
+        }else {
+            return <h1>Infelizmente não posso renderizar isso!</h1>
+        }
+    }
+
     return (
 
         <div>
@@ -10,6 +18,15 @@ const Events = () => {
                 <br/><br/>
                 <button onClick={botaoEvento}>Clique aqui</button>
             </div>
+            <div>
+                <button onClick={()=>{
+                    if(true) {
+                        console.log("Este botão não deveria existir =(")
+                    }
+                }}>Botao não</button>
+            </div>
+            {renderSomething(true)}
+            {renderSomething(false)}
         </div>
     );
 
